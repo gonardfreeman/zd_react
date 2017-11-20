6700f812da885f583f00583c6d937ea14efb1754
Your Name <you@example.com>
2017-11-20 11:09:16 +0200

    prettier rollback

--- zruchna-admin/src/actions/fetching/fetchLogged.js
+++ zruchna-admin/src/actions/fetching/fetchLogged.js
@@ -10,61 +10,61 @@ export const FETCH_LOGIN_SUCCESS = 'FETCH_LOGIN_SUCCESS';
 export const FETCH_LOGIN_ERROR = 'FETCH_LOGIN_ERROR';
 
 function loginRequest() {
-  return {
-    type: FETCH_LOGIN_REQUEST
-  };
+    return {
+        type: FETCH_LOGIN_REQUEST
+    };
 }
 
 function fetchLoginSuccess() {
-  return {
-    type: FETCH_LOGIN_SUCCESS
-  };
+    return {
+        type: FETCH_LOGIN_SUCCESS
+    };
 }
 
 function fetchLoginError(error) {
-  return {
-    type: FETCH_LOGIN_ERROR,
-    error
-  };
+    return {
+        type: FETCH_LOGIN_ERROR,
+        error
+    };
 }
 
 function loginSucces() {
-  return {
-    type: LOGIN_SUCCESS
-  };
+    return {
+        type: LOGIN_SUCCESS
+    };
 }
 
 function loginError(why) {
-  return {
-    type: LOGIN_FAILURE,
-    why
-  };
+    return {
+        type: LOGIN_FAILURE,
+        why
+    };
 }
 
 export function fetchLogin(data) {
-  return dispatch => {
-    dispatch(loginRequest());
-    return fetch(`http://${MAINSITE}/login`, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data),
-      credentials: 'include'
-    })
-      .then(resp => {
-        if (resp.status >= 400) return dispatch(fetchLoginError(resp));
-        return resp.json();
-      })
-      .then(json => {
-        dispatch(fetchLoginSuccess());
-        if (json.status === 200) {
-          dispatch(loginSucces());
-          dispatch(fetchSucces(json));
-        } else {
-          dispatch(loginError(json.status));
-        }
-      });
-  };
+    return dispatch => {
+        dispatch(loginRequest());
+        return fetch(`http://${MAINSITE}/login`, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
+            credentials: 'include'
+        })
+            .then(resp => {
+                if (resp.status >= 400) return dispatch(fetchLoginError(resp));
+                return resp.json();
+            })
+            .then(json => {
+                dispatch(fetchLoginSuccess());
+                if (json.status === 200) {
+                    dispatch(loginSucces());
+                    dispatch(fetchSucces(json));
+                } else {
+                    dispatch(loginError(json.status));
+                }
+            });
+    };
 }
--- zruchna-admin/src/components/UserElements/LoginButton.js
+++ zruchna-admin/src/components/UserElements/LoginButton.js
@@ -5,79 +5,74 @@ import { fetchLogin } from '../../actions/fetching/fetchLogged';
 import login from '../../store/reducers/loginReducers';
 
 class LoginButton extends Component {
-  constructor(props) {
-    super(props);
-    this.login = this.login.bind(this);
-  }
-  login() {
-    const { login, password } = this.props.loginInput;
-    let data = {
-      login: !login ? '' : login,
-      password: !password ? '' : password
-    };
-    this.props.loginFn(data);
-  }
-  render() {
-    const { text } = this.props;
-    const result = login_data => {
-      if (!login_data.is_fetching && !login_data.error && !login.is_logged) {
-        return (
-          <div>
-            <button onClick={this.login}>{text} Test</button>
-          </div>
-        );
-      }
-      if (login_data.is_fetching) {
-        return (
-          <div>
-            <p>Loading...</p>
-          </div>
-        );
-      }
-      if (!login_data.is_logged && !!login_data.error) {
-        return (
-          <div>
-            <button onClick={this.login}>{text}</button>
-            <p className="error">{login_data.error}</p>
-          </div>
-        );
-        if (
-          login.is_logged &&
+    constructor(props) {
+        super(props);
+        this.login = this.login.bind(this);
+    }
+    login() {
+        const { login, password } = this.props.loginInput;
+        let data = {
+            login: !login ? '' : login,
+            password: !password ? '' : password
+        };
+        this.props.loginFn(data);
+    }
+    render() {
+        const { text } = this.props;
+        const result = login_data => {
+            if (!login_data.is_fetching && !login_data.error && !login.is_logged) {
+                return (
+                    <div>
+                        <button onClick={this.login}>{text} Test</button>
+                    </div>
+                );
+            }
+            if (login_data.is_fetching) {
+                return (
+                    <div>
+                        <p>Loading...</p>
+                    </div>
+                );
+            }
+            if (!login_data.is_logged && !!login_data.error) {
+                return (
+                    <div>
+                        <button onClick={this.login}>{text}</button>
+                        <p className="error">{login_data.error}</p>
+                    </div>
+                );
+                if (
+                    login.is_logged &&
           !login.is_fetching &&
           login_data.error.length < 1
-        ) {
-          return (
-            <div>
-              <button onClick={this.login}>{text}</button>
-              <p className="success">Success</p>
-            </div>
-          );
-        }
-      }
-    };
-    return (
-      <div>
-        {/* <button onClick={this.login}>{text}</button> */}
-        {result(this.props.login)}
-      </div>
-    );
-  }
+                ) {
+                    return (
+                        <div>
+                            <button onClick={this.login}>{text}</button>
+                            <p className="success">Success</p>
+                        </div>
+                    );
+                }
+            }
+        };
+        return <div>{result(this.props.login)}</div>;
+    }
 }
 
 function mapStateToProps(state) {
-  const { loginInput, login } = state;
-  return {
-    loginInput,
-    login
-  };
+    const { loginInput, login } = state;
+    return {
+        loginInput,
+        login
+    };
 }
 
 function mapDispatchToProps(dispatch) {
-  return {
-    loginFn: data => {
-      dispatch(fetchLogin(data));
-    }
-  };
+    return {
+        loginFn: data => {
+            dispatch(fetchLogin(data));
+        }
+    };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoginButton);
